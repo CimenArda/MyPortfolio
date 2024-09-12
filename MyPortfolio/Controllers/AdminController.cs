5a9164cf819bdc0d34f65768f779111c54dde118
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-12 15:07:27 +0300

    Layout'un Partial Viewlara Ayrılması

--- MyPortfolio/Controllers/AdminController.cs
+++ MyPortfolio/Controllers/AdminController.cs
@@ -13,5 +13,26 @@ namespace MyPortfolio.Controllers
         {
             return View();
         }
+
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
