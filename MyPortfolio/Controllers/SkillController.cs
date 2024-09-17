1089e79ed7f3404a4e7b452e7e654b0e8f21fa89
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-17 15:59:32 +0300

    Veritabanı Bağlantısı ve Skill Tablosu CRUD İşlemleri

---
+++ MyPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,71 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        MyPortfolioDbEntities context =new MyPortfolioDbEntities();
+
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+		public ActionResult CreateSkill(Skill p)
+		{
+            context.Skill.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+		}
+
+		public ActionResult DeleteSkill(int id)
+        {
+                var skill = context.Skill.Find(id);
+                context.Skill.Remove(skill);
+				context.SaveChanges();
+                return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+		public ActionResult UpdateSkill(int id)
+        {
+             var skill=  context.Skill.Find(id);
+            return View("UpdateSkill",skill);
+        }
+
+		[HttpPost]
+		public ActionResult UpdateSkill(Skill p)
+		{
+			var skill = context.Skill.Find(p.SkillID);
+
+			if (skill == null)
+			{
+				return RedirectToAction("SkillList");
+			}
+
+			skill.Title = p.Title;
+			skill.Icon = p.Icon;
+			skill.Value = p.Value;
+
+			context.SaveChanges();
+
+			return RedirectToAction("SkillList", new { id = p.SkillID });
+		}
+
+
+
+	}
+}
