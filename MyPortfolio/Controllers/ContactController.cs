71efe4bfdf3c06569cecd62c1dccbe2bbc791c62
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-19 15:47:18 +0300

    Contact sınıfı -Inbox,MessageDetails Sayfaları

---
+++ MyPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        MyPortfolioDbEntities context = new MyPortfolioDbEntities();
+
+        public ActionResult Inbox()
+        {
+            var values = context.Contact.ToList();
+            return View(values);
+        }
+
+        public ActionResult MessageDetails(int id)
+        {
+            var value = context.Contact.Where(x=>x.MessageID == id).FirstOrDefault();
+            return View(value);
+
+        }
+
+        public ActionResult MessageStatusChangeToTrue(int id)
+        {
+            var value =context.Contact.Where(x=>x.MessageID == id).FirstOrDefault();
+
+            value.IsRead= true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public ActionResult MessageStatusChangeToFalse(int id)
+        {
+            var value = context.Contact.Where(x => x.MessageID == id).FirstOrDefault();
+
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+    }
+}
